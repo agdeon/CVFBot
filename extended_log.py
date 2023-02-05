2dfd30df178169a2cb994631938eab69e23aff82
Drew <andrew120998@gmail.com>
2023-02-05 13:43:17 +0200

    правки

--- extended_log.py
+++ extended_log.py
@@ -2,19 +2,18 @@ import os
 import time
 import cv2
 import shutil
-
 import constants
 
 
 class ExtendedLog:
     enabled = True
     level = constants.LOG_LVL_COMMON
-
     folder_name = "Logs"
-    image_folder_name = "ImageLog"
+    img_folder_name = "ImageLog"
     filename = 'TextLog.txt'
     img_type = '.png'
 
+    @staticmethod
     def write(level, text, img=None, console=True, exceptional=False):
         if not ExtendedLog.enabled or level > ExtendedLog.level:
             return
@@ -22,7 +21,7 @@ class ExtendedLog:
         log_dir = os.path.join(parent_directory, ExtendedLog.folder_name)
         if not os.path.exists(log_dir):
             os.mkdir(log_dir)
-        image_log_dir = os.path.join(log_dir, ExtendedLog.image_folder_name)
+        image_log_dir = os.path.join(log_dir, ExtendedLog.img_folder_name)
         if not os.path.exists(image_log_dir):
             os.mkdir(image_log_dir)
         if img is not None:
@@ -37,18 +36,21 @@ class ExtendedLog:
         if console:
             print(textlog)
 
-    # устанавливает отображение логов по степени информативности
+    @staticmethod
     def set_level(level):
         ExtendedLog.level = level
 
+    @staticmethod
     def clear():
         path = os.path.join(os.getcwd(), ExtendedLog.folder_name)
         if os.path.exists(path):
             shutil.rmtree(path)
 
+    @staticmethod
     def disable():
         ExtendedLog.enabled = False
-
+    
+    @staticmethod
     def enable():
         ExtendedLog.enabled = True
 
